6a8803c66836800591bbb5493907d9d5a2cd7324
CoreyBrady-dev <cbrady9877@gmail.com>
2025-10-29 14:53:28 -0400

    Implement save and delete task local

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+import json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_FILE = os.path.join(BASE_DIR, "..", "data", "tasks.json")
+
 app = Flask(__name__)
 
 tasks = []
 
+# create tasks
 @app.route("/", methods=["GET", "POST"])
 def home():
+    tasks = load_tasks()
+
     if request.method == "POST":
         title = request.form.get("title")
         priority = request.form.get("priority")
@@ -12,6 +21,28 @@ def home():
 
         if title and priority and description:
             tasks.append({"title": title, "priority": priority, "description": description})
+            save_tasks(tasks)
         return redirect(url_for("home"))
 
     return render_template("index.html", tasks=tasks)
+
+# utility functions
+def load_tasks():
+    try:
+        with open(JSON_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    
+def save_tasks(tasks):
+    with open(JSON_FILE, "w") as f:
+        json.dump(tasks, f, indent=4)
+
+# delete tasks
+@app.route("/delete/<int:index>")
+def delete_task(index):
+    tasks = load_tasks()        
+    if 0 <= index < len(tasks): 
+        tasks.pop(index)        
+        save_tasks(tasks)       
+    return redirect(url_for("home"))  
