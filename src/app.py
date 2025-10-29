a5064802d6d08daac95d97e94426416fcae82ea5
CoreyBrady-dev <cbrady9877@gmail.com>
2025-10-29 13:28:24 -0400

    Implemented basic add tasks

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello, Flask!"
+    if request.method == "POST":
+        title = request.form.get("title")
+        priority = request.form.get("priority")
+        description = request.form.get("description")
+
+        if title and priority and description:
+            tasks.append({"title": title, "priority": priority, "description": description})
+        return redirect(url_for("home"))
+
+    return render_template("index.html", tasks=tasks)
